28f684b76d344a7a076b49a700f67ab365e3dbe6
Uchenna Chilaka <uchenna.chilaka@alliancedata.com>
2018-09-20 15:28:15 -0400

    using dynamic @Input values with *ngFor

--- src/app/passenger-detail/passenger-detail.component.ts
+++ src/app/passenger-detail/passenger-detail.component.ts
@@ -9,10 +9,21 @@ import { Passenger } from '../passenger/passenger.interface';
 export class PassengerDetailComponent implements OnInit {
   @Input()
   item: Passenger;
-
+  editing = false;
   statusDefaultImg = '../../assets/status-filler.png';
 
   constructor() {}
 
   ngOnInit() {}
+
+  onNameChange(value: string) {
+    // console.log(`onNameChange: ${value}`);
+    this.item.fullname = value;
+  }
+
+  toggleEdit() {
+    this.editing = !this.editing;
+  }
+
+  onRemove() {}
 }
